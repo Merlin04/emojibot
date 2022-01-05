3b051d00cf33dac650b87d442d29b2cc8c3453af
merlin04 <Merlin04@users.noreply.github.com>
2022-01-04 19:57:34 -0800

    Format

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,3 +1,3 @@
 // This is where you should export all the features. Every folder in the starter kit follows this spec.
 
-export { default as uploader } from './uploader'
+export { default as uploader } from "./uploader";
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import * as features from "./features/index";
 const app = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     token: process.env.SLACK_BOT_TOKEN,
-	socketMode: true,
-	appToken: process.env.SLACK_APP_TOKEN
+    socketMode: true,
+    appToken: process.env.SLACK_APP_TOKEN,
 });
 
 void (async function () {
