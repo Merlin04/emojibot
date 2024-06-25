f966a885873ce3cf96b9858d7cacba6bb79e6f3f
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-06-25 12:56:00 -0400

    feat: add retry emoji shortcut

---
+++ src/features/retryHandler.ts
@@ -0,0 +1,75 @@
+import fetch from "node-fetch";
+import config from "../config";
+import { SlackApp } from "slack-edge";
+import { humanizeSlackError } from "../utils/translate";
+import { $ } from "bun";
+
+async function reuploadEmoji(emojiName: string, emojiURL: string, user: string) {
+    const form = new FormData();
+    form.append("token", process.env.SLACK_BOT_USER_TOKEN!);
+    form.append("mode", "data");
+    form.append("name", emojiName);
+    const imgBuffer = await fetch(emojiURL, {
+        headers: {
+            Cookie: process.env.SLACK_COOKIE!,
+        },
+    }).then((res) => res.blob());
+
+    const randomUUID = crypto.randomUUID();
+    console.log("writing to tmp", randomUUID);
+    await Bun.write(`tmp/${randomUUID}.png`, imgBuffer);
+    const blob = await Bun.file(`tmp/${randomUUID}.png`);
+
+    form.append("image", blob);
+
+    // No idea how much of this is necessary but I don't feel like figuring it out
+    const res = await fetch(
+        `https://${config.slackWorkspace}.slack.com/api/emoji.add`,
+        {
+            credentials: "include",
+            method: "POST",
+            body: form,
+            headers: {
+                Cookie: `Cookie ${process.env.SLACK_COOKIE}`,
+            },
+        }
+    ).then((res) => res.json() as Promise<{ ok: boolean; error?: string }>);
+
+    await $`rm tmp/${randomUUID}.png`;
+
+    return res.ok
+        ? `:${emojiName}: has been readded, thanks <@${user}>!`
+        : `Failed to readd emoji:
+\`\`\`
+${humanizeSlackError(res)}
+\`\`\``
+}
+
+const feature3 = async (
+    app: SlackApp<{
+        SLACK_SIGNING_SECRET: string;
+        SLACK_BOT_TOKEN: string;
+        SLACK_APP_TOKEN: string;
+    }>
+) => {
+    app.view(
+        "retry_view",
+        async () => { },
+        async ({ context, payload }) => {
+            const meta = JSON.parse(payload.view.private_metadata) as {
+                emoji: string;
+                emojiURL: string;
+                thread_ts: string;
+                user: string;
+            };
+            const status = await reuploadEmoji(meta.emoji, meta.emojiURL, meta.user);
+            await context.client.chat.postMessage({
+                channel: config.channel,
+                thread_ts: meta.thread_ts,
+                text: status,
+            });
+        }
+    );
+};
+
+export default feature3;
---
+++ src/features/retryModal.ts
@@ -0,0 +1,131 @@
+import config from "../config";
+import { ModalView, SlackApp } from "slack-edge";
+
+function retryView(emoji: string, emojiURL: string, thread_ts: string, user: string): ModalView {
+    return {
+        callback_id: "retry_view",
+        type: "modal",
+        title: {
+            type: "plain_text",
+            text: "Confirm Retry",
+        },
+        submit: {
+            type: "plain_text",
+            text: "Confirm",
+            emoji: true,
+        },
+        close: {
+            type: "plain_text",
+            text: "Cancel",
+            emoji: true,
+        },
+        private_metadata: JSON.stringify({ emoji, emojiURL, thread_ts, user }),
+        blocks: [
+            {
+                type: "context",
+                elements: [
+                    {
+                        type: "plain_text",
+                        text: `Are you sure you want to retry uploading :${emoji}:? This will use the same file that was originally uploaded.`,
+                        emoji: true,
+                    }
+                ],
+            },
+            {
+                type: "image",
+                slack_file: {
+                    url: emojiURL,
+                },
+                alt_text: "uploaded emoji file"
+            }
+        ],
+    };
+}
+
+function errorView(reason: string): ModalView {
+    return {
+        type: "modal",
+        title: {
+            type: "plain_text",
+            text: "Error",
+            emoji: true,
+        },
+        close: {
+            type: "plain_text",
+            text: "Okay",
+            emoji: true,
+        },
+        blocks: [
+            {
+                type: "context",
+                elements: [
+                    {
+                        type: "plain_text",
+                        text: reason,
+                        emoji: true,
+                    },
+                ],
+            },
+        ],
+    };
+}
+
+const feature2 = async (
+    app: SlackApp<{
+        SLACK_SIGNING_SECRET: string;
+        SLACK_BOT_TOKEN: string;
+        SLACK_APP_TOKEN: string;
+    }>
+) => {
+    app.shortcut(
+        "retry_emoji",
+        async () => { },
+        async ({ context, payload, body }) => {
+            if (context.channelId !== config.channel) {
+                await context.client.views.open({
+                    trigger_id: payload.trigger_id,
+                    view: errorView(
+                        "This channel doesn't have any emojis managed by emojibot."
+                    ),
+                });
+                return;
+            }
+
+            // check if the user is a workspace admin
+            const isAdmin = await app.client.users
+                .info({
+                    user: body.user.id,
+                })
+                .then((res) => res.user?.is_admin);
+
+            if (
+                body.user.id !== body.message.user &&
+                !config.admins.includes(body.user.id) &&
+                !isAdmin
+            ) {
+                await context.client.views.open({
+                    trigger_id: payload.trigger_id,
+                    view: errorView(
+                        "Only the OP or authorized admins can retry emojis added with emojibot."
+                    ),
+                });
+                return;
+            }
+
+            const emojiName =
+                body.message.text.startsWith(":") &&
+                    body.message.text.endsWith(":")
+                    ? body.message.text.slice(1, -1)
+                    : body.message.text;
+
+            console.log(body.message);
+
+            await context.client.views.open({
+                trigger_id: payload.trigger_id,
+                view: retryView(emojiName, body.message.files[0].url_private!, body.message_ts, body.user.id),
+            });
+        }
+    );
+};
+
+export default feature2;
